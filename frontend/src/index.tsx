9bbde35e5ed35f7db0276104431ddc8d6fad2dfb
陈功 <chengong456@qq.com>
2024-02-01 16:04:27 -0800

    homepage test

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LoginScreen from "./pages/Login/Login"
 import FlashcardViewerFunc from "./pages/FlashcardViewer/App"
+import Homepage from "./pages/Homepage/Homepage"
 
 function App() {
   return (
@@ -12,18 +13,11 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginScreen/>} />
 	<Route path="/flashcard_viewer" element={<FlashcardViewerFunc/>} />
+	<Route path="/" element={<Homepage/>} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-// ReactDOM.render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-//   document.getElementById("root")
-// );
-
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
