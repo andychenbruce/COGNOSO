9dac7354ff2408aca5d1c1d8a1f2e09e6f0f4134
Vince L <viwliu@ucsc.edu>
2024-03-01 14:57:20 -0800

    fixed minmax

--- frontend/src/pages/minigame/index.tsx
+++ frontend/src/pages/minigame/index.tsx
@@ -95,7 +95,7 @@ const App: React.FC = () => {
         setRightCard(droppedCard);
       };
 
-      function getRandomInt(min, max) {
+      function getRandomInt(min: number, max: number) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
