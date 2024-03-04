6100e799ac1c284fd1d4b2e2e84003fccb784634
Vince L <viwliu@ucsc.edu>
2024-03-03 22:57:34 -0800

    more icons added 2

--- frontend/src/pages/home_page/index.tsx
+++ frontend/src/pages/home_page/index.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "../../navbar";
 import "./home.css";
 import StarIcon from '@mui/icons-material/Star';
 import ViewCarouselIcon from '@mui/icons-material/ViewCarousel';
-
+import ViewCarouselTwoToneIcon from '@mui/icons-material/ViewCarouselTwoTone';
 const App: React.FC = () => {
   return (
     <div className="App">
@@ -14,8 +14,12 @@ const App: React.FC = () => {
         <div className="Content-box Favorites-box">{}</div>
       </div>
       <div className="Decks">
-        <h2> {<ViewCarouselIcon />} Decks</h2>
+        <h2> {<ViewCarouselTwoToneIcon />}Decks</h2>
         <div className="Content-box Decks-box">{}</div>
+      </div>
+      <div className="OtherDecks">
+        <h2> {<ViewCarouselIcon />} Decks By Other Users</h2>
+        <div className="Content-box OtherDecks-box">{}</div>
       </div>
     </div>
   );
