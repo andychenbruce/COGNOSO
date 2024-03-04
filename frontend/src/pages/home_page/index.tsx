f4c9515a506bb1c1c4d5a03f3ef31fcf97894382
Vince L <viwliu@ucsc.edu>
2024-03-03 22:57:20 -0800

    more icons

--- frontend/src/pages/home_page/index.tsx
+++ frontend/src/pages/home_page/index.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Navbar } from "../../navbar";
 import "./home.css";
+import StarIcon from '@mui/icons-material/Star';
+import ViewCarouselIcon from '@mui/icons-material/ViewCarousel';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Navbar />
       <div className="Favorites">
-        <h2>Favorites</h2>
+        
+        <h2>{<StarIcon />} Favorites</h2>
         <div className="Content-box Favorites-box">{}</div>
       </div>
       <div className="Decks">
-        <h2>Decks</h2>
+        <h2> {<ViewCarouselIcon />} Decks</h2>
         <div className="Content-box Decks-box">{}</div>
       </div>
     </div>
